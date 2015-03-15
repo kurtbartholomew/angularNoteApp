96bf7b979d03e3268624501a08508514dfbd72dd
Kurt Bartholomew <siever246@gmail.com>
2015-03-15 12:09:29 -0700

    Added a user factory as well as templates and controllers for user

--- app/assets/javascript/controllers/notesCreateController.js
+++ app/assets/javascript/controllers/notesCreateController.js
@@ -1,4 +1,16 @@
 angular.module('NoteWrangler')
-.controller('NotesEditController', function() {
-	
+.controller('NotesCreateController', function($scope, Note, $location) {
+	$scope.note = new Note();
+	$scope.isSubmitting = false;
+
+	$scope.saveNote = function(note){
+		$scope.isSubmitting = true;
+		note.$save().then(function(){
+			$location.path('/notes');
+		}).catch(function(errors){
+			//validations 
+		}).finally(function() {
+			$scope.isSubmitting = false;
+		});
+	};
 });
