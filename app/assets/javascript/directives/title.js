bd8f4bd1442f73e3731484a78730bc584da0df92
Kurt Bartholomew <siever246@gmail.com>
2015-03-15 17:24:15 -0700

    Implemented a gravatar module for users

--- app/assets/javascript/directives/title.js
+++ app/assets/javascript/directives/title.js
@@ -1,12 +1,12 @@
 angular.module("NoteWrangler")
-.directive('title', function() {
-  return function(scope, element, attrs, $timeout) {
+.directive('title', function($timeout) {
+  return function(scope, element, attrs) {
     $timeout(function(){
       $(element).tooltip();
     });
 
     scope.$on('$destroy', function() {
-       $element.tooltip('destroy');
+       $(element).tooltip('destroy');
     });
   }
 });
